c7adb50a84cfb3428c2a27d747af156bcf834515
toanmtph30393 <toanmtph30393@fpt.edu.vn>
2024-09-21 06:45:39 -0400

    Revert ""
    This reverts commit fd63750a687564740a4688b59dc04a4281f97266.

--- src/main/java/com/n2/sprintburst/response/HoaDonChiTietResponse.java
+++ src/main/java/com/n2/sprintburst/response/HoaDonChiTietResponse.java
@@ -1,23 +1,49 @@
+<<<<<<< Updated upstream
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.n2.sprintburst.response;
 
+/**
+ *
+ * @author NhokHip
+ */
+public class HoaDonChiTietResponse {
+    
+}
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.n2.sprintburst.response;
+
+import com.n2.sprintburst.entity.HoaDon;
+import com.n2.sprintburst.entity.SanPhamChiTiet;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@NoArgsConstructor
+/**
+ *
+ * @author NhokHip
+ */
 @AllArgsConstructor
-@Setter
-@Getter
-
+@NoArgsConstructor
+@Getter 
+@Setter 
+@ToString
+@Builder
 public class HoaDonChiTietResponse {
-
     private int id;
     private int soLuong;
     private int giaBan;
     private boolean trangThai;
-    private int idhoaDon;
-    private int idsanPhamChiTiet;
-
-    
+    private HoaDon hoaDon;
+    private SanPhamChiTiet sanPhamChiTiet;
 }
+>>>>>>> Stashed changes
