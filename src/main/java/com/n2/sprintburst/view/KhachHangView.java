539d46f83b5075c96f7524e0794a9b8fb44a9ffb
toanmtph30393 <toanmtph30393@fpt.edu.vn>
2024-09-21 07:15:50 -0400

    Revert "change"

--- src/main/java/com/n2/sprintburst/view/KhachHangView.java
+++ src/main/java/com/n2/sprintburst/view/KhachHangView.java
@@ -50,7 +50,6 @@ public class KhachHangView extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
